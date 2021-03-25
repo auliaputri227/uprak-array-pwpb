fff6bcb3cb438c9b87bf14f2e9abec1d1cb23a2d
Aulia Putri <68450757+auliaputri227@users.noreply.github.com>
2021-03-25 14:47:29 +0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,117 @@
+<?php
+include 'koneksi.php';
+
+if(isset($_POST["login"])){
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+
+  if($username === "admin" &&  $password === "123"){
+      header("Location: index.php"); 
+  } else{
+    echo"<script>alert('Masukan username dan password yang benar !!')</script>;";
+    }
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>Login &mdash; Stisla</title>
+
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+  <!-- CSS Libraries -->
+  <link rel="stylesheet" href="../node_modules/bootstrap-social/bootstrap-social.css">
+
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="../assets/css/style.css">
+  <link rel="stylesheet" href="../assets/css/components.css">
+</head>
+
+<body style="background-color: 	#DCDCDC;" >
+  <div id="app">
+    <section class="section">
+      <div style="padding-top: 100px; " class="container mt-5">
+        <div class="row">
+          <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+            <div class="card card-primary">
+              <div class="card-header text-center"><h4>PORTAL SMK TB</h4></div>
+              <div class="card-body">
+                <form  action="" method="POST" class="needs-validation" novalidate="">
+                <input type="hidden" hidden name="id" value="<?= $siswa['id'] ?>">
+                  <div class="form-group">
+                    <input id="username" type="text" placeholder="Username" class="form-control" name="username" tabindex="1" required autofocus>
+                    <div class="invalid-feedback">
+                      Please fill in your Username
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <input id="password" type="password" placeholder="Password" class="form-control" name="password" tabindex="2" required>
+                    <div class="invalid-feedback">
+                      please fill in your password
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="custom-control custom-checkbox">
+                      <input type="checkbox" name="remember" class="custom-control-input" tabindex="3" id="remember-me">
+                      <label class="custom-control-label" for="remember-me">Remember Username</label>
+                    </div>
+                  </div>
+                  <input type="submit" name="login" value="login" class="btn btn-primary btn-lg btn-block">
+                </form>
+                <div class="text-center mt-2">
+                    <a href="auth-forgot-password.html" class="text-small">
+                        Forgotten your username or password??
+                    </a>
+                </div>
+                
+                <div class="row sm-gutters mt-2">
+                  <div class="col-6">
+                    <a class="btn btn-block btn-social btn-facebook">
+                      <span class="fab fa-facebook"></span> Facebook
+                    </a>
+                  </div>
+                  <div class="col-6">
+                    <a class="btn btn-block btn-social btn-instagran">
+                      <span class="fab fa-instagram"></span> Instagram
+                    </a>
+                  </div>
+                </div>
+
+                
+              </div>
+            </div>
+            <div class="text-center mt-5"><h4>PORTAL SMK TB</h4></div>
+            <div class="simple-footer text-center">
+              Copyright By Aulia Putri
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+  </div>
+
+  <!-- General JS Scripts -->
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+  <script src="../assets/js/stisla.js"></script>
+
+  <!-- JS Libraies -->
+
+  <!-- Template JS File -->
+  <script src="../assets/js/scripts.js"></script>
+  <script src="../assets/js/custom.js"></script>
+
+  <!-- Page Specific JS File -->
+</body>
+</html>
